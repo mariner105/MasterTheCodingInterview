e69e09007fd912e489820b63376b2d984d5168e5
Frank Hundley <mariner105@msn.com>
2021-10-20 18:27:48 -0700

    N squared example

--- src/main/java/exercises/LogAllPairs.java
+++ src/main/java/exercises/LogAllPairs.java
@@ -7,16 +7,23 @@ public class LogAllPairs {
     private static final int[] boxes = {1,2,3,4,5};
 
     public static void main(String[] args) {
-        logAllPairsOfArray(boxes);
+        logAllPairsOfArrayN2(boxes);
     }
 
 
-    public static void logAllPairsOfArray(int[] boxes) {
+    /**
+     * O(n^2) example (This has bad efficiency)
+     * @param boxes
+     */
+    public static void logAllPairsOfArrayN2(int[] boxes) {
         int index1 = 0;
         int index2 = index1 + 1;
 
-        while (index1 < boxes.length && index2 < boxes.length) {
-            System.out.println(boxes[index1++] + ", " + boxes[index2++]);
+        for (int i = 0; i < boxes.length - 1; i++){
+            for (int j = 1; j < boxes.length; j++) {
+                System.out.println(boxes[i] + ", " + boxes[j]);
+            }
         }
     }
+
 }
