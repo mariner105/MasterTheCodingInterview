5901be2c32199ae5bc76a0bbd6a2e6f98b726e37
Frank Hundley <mariner105@msn.com>
2021-10-24 07:05:21 -0700

    Minor update

---
+++ src/main/java/exercises/ArrayFindFirstAndLast.java
@@ -0,0 +1,17 @@
+package exercises;
+
+/**
+ * Find 1st, find Nth
+ */
+public class ArrayFindFirstAndLast {
+
+    public static void main(String[] args) {
+        String[] array = {"hi", "my", "teddy"};
+
+        String first = array[0]; // O(1)
+        String last = array[array.length - 1]; // O(1)
+
+    }
+
+
+}
