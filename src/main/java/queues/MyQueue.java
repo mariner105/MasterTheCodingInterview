a738427c6639e94e7c3e38f834a3715f956d849e
Frank Hundley <mariner105@msn.com>
2021-10-28 17:04:32 -0700

    Added MyQueue

---
+++ src/main/java/queues/MyQueue.java
@@ -0,0 +1,36 @@
+package queues;
+
+/**
+ * This is a FIFO data structure.
+ *
+ */
+public class MyQueue {
+
+    /**
+     * O(n)
+     */
+    public void lookup() {
+
+    }
+
+    /**
+     * O(1)
+     */
+    public void enqueue() {
+
+    }
+
+    /**
+     * O(1)
+     */
+    public void dequeue() {
+
+    }
+
+    /**
+     * O(1)
+     */
+    peek() {
+
+    }
+}
